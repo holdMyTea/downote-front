8b252005be1f095ca3f23abe20a52f718cf1623a
rise42 <oleksandr.pohrebniak42@gmail.com>
2019-08-10 11:47:08 +0300

    Override location for correct assertions

--- src/__tests__/containers/Login.test.js
+++ src/__tests__/containers/Login.test.js
@@ -1,36 +1,62 @@
-import '@testing-library/react/cleanup-after-each'
 import '@testing-library/jest-dom/extend-expect'
 import { fireEvent, findByText } from '@testing-library/react'
 
 import React from 'react'
 
-import { renderWithRedux, flushAllPromises } from '../../../test/utils'
+import { history, renderWithRedux, flushAllPromises } from '../../../test/utils'
 import Login from '../../containers/Login/Login'
 
-test('Allows succesful login', async () => {
-  // mocking login api call
-  jest.spyOn(global, 'fetch').mockResolvedValue({ // mocking successful fetch
-    json: () => ({ token: '123123123' }),
-    ok: true,
-    code: 200
+describe('Login component', () => {
+  beforeEach(() => history.push('/')) // resetting location to root
+
+  it('Logs in on 200', async () => {
+    jest.spyOn(global, 'fetch').mockResolvedValueOnce({ // mocking successful fetch
+      json: () => ({ token: '123123123' }),
+      ok: true,
+      code: 200
+    })
+
+    // render
+    const { getByPlaceholderText, getByText } = renderWithRedux(
+      (<Login />)
+    )
+
+    // filling out the form
+    fireEvent.change(getByPlaceholderText(/Email/i), { target: { value: 'kappa@mail.com' } })
+    fireEvent.change(getByPlaceholderText(/Password/i), { target: { value: '123123' } })
+
+    fireEvent.click(getByText(/Log In/i)) // submitting
+
+    expect(findByText('Woopsi-Doopsie')).toBeTruthy() // loading message is shown
+
+    await flushAllPromises() // awaiting request processed
+
+    expect(location.pathname).toBe('/home') // checking the user was redirected after login
   })
 
-  // render
-  const { getByPlaceholderText, getByText } = renderWithRedux(
-    (<Login />)
-  )
+  it('Shows wrong credentials message on 401', async () => {
+    jest.spyOn(global, 'fetch').mockResolvedValueOnce({ // mocking failed fetch
+      json: () => ({ error: 'Wrong login credentials' }),
+      ok: false,
+      code: 401
+    })
 
-  // filling out the form
-  fireEvent.change(getByPlaceholderText(/Email/i), { target: { value: 'kappa@mail.com' } })
-  fireEvent.change(getByPlaceholderText(/Password/i), { target: { value: '123123' } })
+    // render
+    const { getByPlaceholderText, getByText } = renderWithRedux(
+      (<Login />)
+    )
 
-  // submitting
-  fireEvent.click(getByText(/Log In/i))
+    // filling out the form
+    fireEvent.change(getByPlaceholderText(/Email/i), { target: { value: 'wrong@mail.com' } })
+    fireEvent.change(getByPlaceholderText(/Password/i), { target: { value: 'wrongg' } })
 
-  expect(findByText('Woopsi-Doopsie')).toBeTruthy() // loading message is shown
+    fireEvent.click(getByText(/Log In/i)) // submitting
 
-  await flushAllPromises() // awaiting request processed
+    expect(findByText('Woopsi-Doopsie')).toBeTruthy() // loading message is shown
 
-  expect(location.pathname).toBe('/home') // checking the user was redirected after login
-  expect(global.fetch).toHaveBeenCalledTimes(1)
+    await flushAllPromises() // awaiting request processed
+
+    expect(location.pathname).toBe('/') // checking the user was NOT redirected
+    expect(findByText('Wrong login credentials')).toBeTruthy() // finding error notification
+  })
 })
--- test/utils.js
+++ test/utils.js
@@ -1,13 +1,18 @@
 import React from 'react'
 import { createStore, applyMiddleware } from 'redux'
-import { BrowserRouter } from 'react-router-dom'
+import { Router } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
 
 import { render } from '@testing-library/react'
+import { createBrowserHistory } from 'history'
 
 import reducers from '../src/reducers'
 
+// exposing history object, to be able to flush it to root in beforeEach
+export const history = createBrowserHistory({ initialEntries: ['/'] })
+
+// render function with all the Providers
 export const renderWithRedux = (
   component,
   initialState = {},
@@ -16,14 +21,15 @@ export const renderWithRedux = (
   const store = createStore(reducer, initialState, applyMiddleware(thunk))
   return {
     ...render(
-      <BrowserRouter>
+      <Router history={history}>
         <Provider store={store}>
-          {component}
+          { component }
         </Provider>
-      </BrowserRouter>
+      </Router>
     ),
     store
   }
 }
 
-export const flushAllPromises = () => new Promise(resolve => setImmediate(resolve))
+// immediate resolve for pending promises
+export const flushAllPromises = () => new Promise(resolve => setImmediate(resolve), reject => setImmediate(reject))
