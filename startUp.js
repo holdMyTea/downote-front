02030d2a95147bcd7ca8bc0eb9e66bbfddcf9e4d
holdMyTea <oleksandr.pohrebniak42@gmail.com>
2020-01-09 16:31:42 -0500

    Start-up patch to parse API_HOST env

--- startUp.js
+++ startUp.js
@@ -1,12 +1,23 @@
 const { exec } = require('child_process')
 const { writeFileSync } = require('fs')
 
-exec('getent hosts api | awk \'{ print $1 }\'', (err, stdout) => {
-  if (err) {
-    console.error(err)
-  } else {
-    const URL = `${stdout.trimEnd()}:${process.env.API_PORT}`
-    writeFileSync('./.env', `REACT_APP_API=${URL}`)
-    console.log('API URL is set to: ', URL)
-  }
-})
+// A start up script being run right before npm start
+// Checks envs and sets a new env to determine API URL
+
+let URL
+
+if (process.env['API_HOST']) { // if $API_HOST is set, using it
+  URL = `${process.env['API_HOST']}:${process.env.API_PORT}`
+} else { // if not getting `api` IP from system's hosts
+  exec('getent hosts api | awk \'{ print $1 }\'', (err, stdout) => {
+    if (err) {
+      console.error(err)
+      process.exit(0)
+    }
+    URL = `${stdout.trimEnd()}:${process.env.API_PORT}`
+  })
+}
+
+// writing new env that will be included into CRA process
+writeFileSync('./.env', `REACT_APP_API=${URL}`)
+console.log('API URL is set to: ', URL)
